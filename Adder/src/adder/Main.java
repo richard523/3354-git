4ef11d8d4a99f4922ca83bf20ced68b6c7f44cd2
richard523 <rgatchalian523@gmail.com>
2020-09-24 23:39:27 -0500

    experimental subtract of adder
    Changes to adder

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -8,24 +8,48 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+           
+            
+            System.err.println("Please enter integers");
+            
+            
+
         }
     }
 
     private static int addArguments(String[] args) {
-        int howMany, num1, num2, sum;
+        int howMany = -1;
+        int num1, sum;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter how many inputs: ");
-        // howMany = sc.nextInt();
         sum = 0;
-        for (howMany = sc.nextInt(); howMany > 0; howMany--) {
-            num1 = 0;
-            System.out.println("Enter number " + howMany);
-            num1 = sc.nextInt();
-            sum += num1;
+        System.out.println("Enter how many inputs or negative how many: ");
+        // sc.skip("-");
+        howMany = sc.nextInt();
+        
+        if (howMany <= -1){
+            howMany =(-1) * howMany;
+            
+            for (; howMany > 0; howMany--) {
+                num1 = 0;
+                System.out.println("Enter number " + howMany);
+                num1 = sc.nextInt();
+                sum -= num1;
+                
+            }
+            sc.close();
         }
-        
-        
+        else if (howMany > 0) {
+            howMany = sc.nextInt();
+            for (; howMany > 0; howMany--) {
+                num1 = 0;
+                System.out.println("Enter number " + howMany);
+                num1 = sc.nextInt();
+                sum += num1;
+                
+            }
+            sc.close();
+        } 
+  
 
 
         sc.close();
